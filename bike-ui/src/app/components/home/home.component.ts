5dca71cca7bdbc39c921049b4237d9a460238da9
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-08-18 15:18:10 +0400

    add UIv3

--- bike-ui/src/app/components/home/home.component.ts
+++ bike-ui/src/app/components/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {ServiceService} from "../../services/service.service";
 import {FormBuilder, FormControl,  Validators} from "@angular/forms";
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
